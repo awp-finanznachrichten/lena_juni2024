51818cf01737ae869c20b264a4a02767156afecd
awp-finanznachrichten <sw@awp.ch>
2024-06-09 12:35:56 +0200

    commit from Rstudio

--- LENA_RUN.R
+++ LENA_RUN.R
@@ -1,4 +1,4 @@
-#repeat{
+repeat{
 MAIN_PATH <- "C:/Users/simon/OneDrive/LENA_Project/20240609_LENA_Abstimmungen"
 
 #Working Directory definieren
@@ -76,7 +76,7 @@ source("commit.R", encoding="UTF-8")
   
 if (time_check_national == FALSE) {
 #Tabellen aktualisieren
-#source("votations_juin_2024/top_flop/top_flop_run.R", encoding="UTF-8")
+source("votations_juin_2024/top_flop/top_flop_run.R", encoding="UTF-8")
 }
 
 #Timestamp speichern
@@ -93,5 +93,5 @@ cat(time_end-time_start)
 }
 
 #Sys.sleep(10)
-#}
+}
 
